892b33f4fecb47bda5a3531de99147ca4e33deea
thanhloi <nhoxzunbmt@gmail.com>
2018-05-18 17:47:00 +0700

    u

--- learn/views.py
+++ learn/views.py
@@ -22,25 +22,31 @@ def home(request):
     
     # bloger1 = Bloger.objects.get(pk=1)
 
-    # en =  Entry()
-    # en.bloger = bloger1;
-    # en.headline = 'a'
-    # en.body_text = 'Abc ' 
-    # en.pub_date = datetime.now(tz=None)
-    # en.mod_date = datetime.now(tz=None)
-    # en.n_comments = 1
-    # en.n_pingbacks = 1
-    # en.rating = 1
-    # en.save()
+    # for  x in range(0,99):
+    #     body = 'Entry ' + str(x)
+    #     en =  Entry()
+    #     en.bloger = bloger1;
+    #     en.headline = body
+    #     en.body_text = body
+    #     en.pub_date = datetime.now(tz=None)
+    #     en.mod_date = datetime.now(tz=None)
+    #     en.n_comments = 1
+    #     en.n_pingbacks = 1
+    #     en.rating = 1
+    #     en.save()
 
-    q1 = Entry.objects.filter(headline__startswith='a')
+    # q1 = Entry.objects.filter(headline__startswith='a')
     
 
+      
+    # Entry.objects.order_by('headline')[0]
+
+    # Entry.objects.filter(pub_date__lte='2018-05-14')
+
+    # Entry.objects.filter(headline__exact="1")
+
     
-
-
     
-
     
     
     response = JsonResponse({'status' : 'success'})
