f2ec1d03f8d07222929b79f58143d48c1732418b
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2018-05-29 22:48:30 +0700

    u

--- learn/views.py
+++ learn/views.py
@@ -65,3 +65,6 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def upload_file(request):
+    return '123'
